b3ad343d787b61dd3422f3007ce9fa7311247c52
kamani-in <ksep2102@gmail.com>
2026-02-20 12:45:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model = pickle.load(open("random_forest_model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        features = [float(x) for x in request.form.values()]
+        prediction = model.predict([features])
+
+        if prediction[0] == 1:
+            result = "Startup is Likely to Succeed 🚀"
+        else:
+            result = "Startup is Likely to Fail ⚠"
+
+        return render_template("index.html", prediction_text=result)
+
+    except:
+        return render_template("index.html", prediction_text="Error in Input")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5005)
